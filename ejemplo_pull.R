9b8f4ea34c080d83dd3b907ac8925afd31fc2a6c
Brenda119 <grape16.98@gmail.com>
2021-08-31 01:35:54 +0000

    Realizamos una actualización al código para obtener un summary de la base de datos iris.

--- ejemplo_pull.R
+++ ejemplo_pull.R
@@ -1,5 +1,8 @@
-#Descargas script de R desde Github hacia R Studio Cloud
+#Descargar script de R desde Github hacia R Studio Cloud
 
 iris <- iris
 
 head(iris)
+
+#Agregar nuevas lineas de codigo para actualizar
+summary(iris)
